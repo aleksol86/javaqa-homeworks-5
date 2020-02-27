c226027ec12a96e4135ed68f12e21f7ec27ba311
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-02-27 23:15:07 +0300

    Добавлен java-код

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 5200;
+        int bonus = 20;
+        int mileQuantity = 5200 / 20;
+        System.out.println(mileQuantity);
+    }
+
+}
